8524f47c5e77f49293351bd0183239cb986cc9a4
Kanav-ar <kanava879@gmail.com>
2026-07-28 18:58:49 +0530

    feat: refactor Navbar links for active state styling; enhance NotFound page with user-friendly message and navigation button; add new About components

--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -1,9 +1,17 @@
+import { Link } from "react-router";
+import BlueBtn from "../components/common/BlueBtn";
+
 export default function NotFound() {
   return (
-    <>
-      <h1 className="text-white text-4xl font-bold text-center">
-        Notfound
-      </h1>{" "}
-    </>
+    <div className="h-screen flex justify-center items-center">
+      <div className="flex flex-col items-center gap-12">
+        <h1 className="text-gray-700 dark:text-white text-4xl font-bold text-center">
+          Page Not Found !
+        </h1>{" "}
+        <Link to={"/"}>
+          <BlueBtn tag="Go Home" />
+        </Link>
+      </div>
+    </div>
   );
 }
