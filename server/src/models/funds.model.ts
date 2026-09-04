9d8448bfe1ad4980c1ff82d6e3c4fd5a7529cfff
Kanav-ar <kanava879@gmail.com>
2026-09-04 18:33:34 +0530

    feat: implement addFunds and withdrawFunds functionalities; update fund routes with authentication

--- server/src/models/funds.model.ts
+++ server/src/models/funds.model.ts
@@ -1,31 +1,34 @@
 import mongoose from "mongoose";
 
-const fundsSchema = new mongoose.Schema({
-  availableCash: {
-    type: Number,
-    default: 0,
-  },
+const fundsSchema = new mongoose.Schema(
+  {
+    availableCash: {
+      type: Number,
+      default: 0,
+    },
 
-  usedMargin: {
-    type: Number,
-    default: 0,
-  },
+    usedMargin: {
+      type: Number,
+      default: 0,
+    },
 
-  payin: {
-    type: Number,
-    default: 0,
-  },
+    payin: {
+      type: Number,
+      default: 0,
+    },
 
-  openingBalance: {
-    type: Number,
-    default: 0,
+    openingBalance: {
+      type: Number,
+      default: 0,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true,
+    },
   },
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-    unique: true,
-  },
-});
+  { timestamps: true },
+);
 
 export const Fund = mongoose.model("Fund", fundsSchema);
--- server/src/routes/fund.routes.ts
+++ server/src/routes/fund.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
 import { authenticate } from "../middlewares/auth.middleware";
+import {
+  addFunds,
+  getFunds,
+  withdrawFunds,
+} from "../controllers/funds.controllers";
 
 const fundRouter = Router();
 
-fundRouter.route("/").get(authenticate)
-fundRouter.route("/add").post(authenticate)
-fundRouter.route("/withdraw").post(authenticate)
+fundRouter.route("/").get(authenticate, getFunds);
+fundRouter.route("/add").post(authenticate, addFunds);
+fundRouter.route("/withdraw").post(authenticate, withdrawFunds);
